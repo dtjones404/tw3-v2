625c4e38bbbe2ebd5d6f7df8567b257694dde9f7
dtjones404 <dustintaylorjones@gmail.com>
2022-06-04 22:07:50 -0400

    Debugging dependencies for Vercel.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import nextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 import SequelizeAdapter from '@next-auth/sequelize-adapter';
 import { Sequelize } from 'sequelize';
+import pg from 'pg';
 
 const sequelize = new Sequelize(process.env.DB_URI!);
 
