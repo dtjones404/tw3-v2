9477931d1c9f7ad947acd87eaadecc733ae01f6c
dtjones404 <dustintaylorjones@gmail.com>
2022-06-07 15:55:01 -0400

    Created contact/privacy pages, File model.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,27 @@
+import Link from 'next/link';
+
 export default function Footer() {
   return (
     <footer className="mt-8 bg-stone-900 text-white py-12 shadow-xl">
-      <div className="container mx-auto">Footer</div>
+      <div className="container mx-auto">
+        <ul className="flex justify-center items-center gap-12">
+          <li>
+            <Link href="/contact">
+              <a>Contact us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/terms">
+              <a>Terms of Use</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/privacy">
+              <a>Privacy Policy</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 }
